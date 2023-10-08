b6dc79ca9aaeb3a0f16f15a806d744e75bbf53c1
thepetras <thepetras@gmail.com>
2023-10-07 23:39:23 -0300

    Adicionando MinhaClasse e modificando AboutMe

---
+++ Calculadora/src/Calculadora.java
@@ -0,0 +1,24 @@
+//**
+* <h1>Calculadora</h1>
+* A calculadora realiza operações matemáticas entre números
+* <p>
+* <b> Note:</b> Leia atentamente a documentação desta classe
+* para desfrutar  dos recursos oferecidos pelo autor
+*
+* @author thepetras
+* @version 1.0
+* @since 01/01/2023
+*/
+
+public class Calculadora{
+    /**
+    * Este método é utilizado para somar dois números inteiros
+    * @param numeroUm este é o primeiro parâmetro do método
+    * @param numeroDois  este é o segundo parâmetro do método
+    * @return int o resultado deste método é a soma dos dois números.
+    */
+
+    public int somar(int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+    }
+}
